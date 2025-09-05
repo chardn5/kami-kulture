c176fce0bf39597cff397e615dd2526abf13bae8
Richard Noceda <richard.noceda@gmail.com>
2025-09-05 14:29:30 +0800

    CheckoutForm + PayPal gating + capture route + admin orders

---
+++ prisma/migrations/20250905062828_add_shipping_snapshot/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "public"."Order" ADD COLUMN     "shipAddress1" TEXT,
+ADD COLUMN     "shipAddress2" TEXT,
+ADD COLUMN     "shipCity" TEXT,
+ADD COLUMN     "shipCountry" TEXT,
+ADD COLUMN     "shipEmail" TEXT,
+ADD COLUMN     "shipFirstName" TEXT,
+ADD COLUMN     "shipLastName" TEXT,
+ADD COLUMN     "shipPhone" TEXT,
+ADD COLUMN     "shipPostalCode" TEXT,
+ADD COLUMN     "shipState" TEXT;
